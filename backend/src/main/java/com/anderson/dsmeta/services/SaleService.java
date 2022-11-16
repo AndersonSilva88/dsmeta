0a9d5375ff18d67dcf86997fbf40f04f66f159a7
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2022-11-16 09:35:52 -0300

    API test

---
+++ backend/src/main/java/com/anderson/dsmeta/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.anderson.dsmeta.services;
+
+import com.anderson.dsmeta.entities.Sale;
+import com.anderson.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale> findSales() {
+        return repository.findAll();
+    }
+}
